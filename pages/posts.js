1bccc0d4a953a93e17ec45d4438dacd2d04f43c0
dylan <dylanabozarth@gmail.com>
2021-02-24 11:34:15 -0800

    Css stuff

--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,16 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
+import Head from 'next/head'
+import Layout from './components/layout'
 export default function FirstPost() {
-    return <h1><a href="/">Back to home</a></h1>
+    return <Layout>
+        <Head>
+        <title>First Post</title> {/* controls title */}
+      </Head><Link href="/">Back gome</Link>
+      <br /><Image
+    src="/images/doge.jpg" // Route of the image file
+    height={500} // Desired size with correct aspect ratio
+    width={1000} // Desired size with correct aspect ratio
+    alt="Your Name"
+  /></Layout>
   }
