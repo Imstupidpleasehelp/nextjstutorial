2eb97a8faafc1a080c8f813b1696b275603e1b03
dylan <dylanabozarth@gmail.com>
2021-02-26 13:41:57 -0800

    more like a blog

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,80 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout from './components/layout'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "./components/layout";
 export default function Home() {
   return (
     <Layout>
-    <div className="container">
-      <Head>
-        <title>Peter Frampton rocks</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          BABABDOOEY 
-        </h1>
-
-        <Link href="/posts">
-    <a>this page!</a>
-  </Link>
-
+      <div className="container">
+        <Head>
+          <title>DogeBlog, all about Doge</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
         
-      </main>
+          <h1 className="title">DogeBlog</h1>
+          <p className="subtitle">Wow!</p>
+          <Image
+            src="/images/dogetitle.jpg" 
+            height={144} // Desired size with correct aspect ratio
+            width={204} // Desired size with correct aspect ratio
+          ></Image>
+          <Link href="/posts">
+            <a className="subtitle">See posts</a>
+          </Link>
+       
 
-      <footer>
-        
-          Powered by{' '}
-         JESUS
-        
-      </footer>
+        <footer className="footer">Powered by pure meme energy</footer>
 
-      <style jsx>{`
+        <style jsx>{`
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
           display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          background-color: white;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
             flex-direction: column;
+           
+            align-items: center;
+        }
+          .title {
+            color: white;
           }
-        }
-      `}</style>
+.subtitle {
+  color: white;
+  font-weight: bolder;
+  font-size: 2rem;
+  
+}
+          
+.footer {
+  text-align:center;
+  font-size:1rem;
+  font-weight: bolder;
+  bottom: 0;
+  position: fixed;
+}
+          @media (max-width: 600px) {
+            .grid {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+        <style jsx global>{`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+              
+          }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+          * {
+            box-sizing: border-box;
+          }
+        `}</style>
+      </div>
     </Layout>
-  )
+  );
 }
--- pages/posts.js
+++ pages/posts.js
@@ -2,15 +2,19 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 import Layout from './components/layout'
+
 export default function FirstPost() {
     return <Layout>
         <Head>
         <title>First Post</title> {/* controls title */}
-      </Head><Link href="/">Back gome</Link>
+      </Head><Link href="/" >Back gome</Link> 
+      <p className="center">AAAAAAAAAAAAAA</p>
       <br /><Image
     src="/images/doge.jpg" // Route of the image file
     height={500} // Desired size with correct aspect ratio
     width={1000} // Desired size with correct aspect ratio
     alt="Your Name"
-  /></Layout>
+  />
+  </Layout>
   }
+ 
